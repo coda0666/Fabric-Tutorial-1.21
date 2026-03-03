19ccf8eb9b8a13728e1bf3863c5db67b6fa5fede
mhoff <mayahoffmann2005@gmail.com>
2026-03-02 18:00:55 -0600

    init commit

---
+++ src/main/java/net/coda/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,9 @@
+package net.coda.tutorialmod.block;
+
+import net.coda.tutorialmod.TutorialMod;
+
+public class ModBlocks {
+    public static void registerModBlocks() {
+        TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
+    }
+}
